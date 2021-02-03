7ea8c7262a91132f59a8b7ab48cee0c504ccdfb2
Hugo Manrique <contact@hugmanrique.me>
2021-02-03 18:42:10 +0100

    Ignore null terminators by default

--- src/main/java/me/hugmanrique/cartage/util/StringUtils.java
+++ src/main/java/me/hugmanrique/cartage/util/StringUtils.java
@@ -22,19 +22,56 @@ public final class StringUtils {
   }
 
   /**
-   * Ensures the given string is composed of uppercase ASCII characters and spaces.
+   * Ensures the given string is composed of uppercase ASCII characters, spaces, and
+   * null terminators ({@code \0}).
    *
    * @param string the string to check
    * @throws IllegalArgumentException if the string contains non-ASCII or non-uppercase characters
    */
   public static void requireUppercaseAscii(final String string) {
+    requireUppercaseAscii(string, true);
+  }
+
+  /**
+   * Ensures the given string is composed of uppercase ASCII characters and spaces, ignoring
+   * null terminators ({@code \0}) if specified.
+   *
+   * @param string the string to check
+   * @param acceptNullTerminators whether to ignore null terminators
+   * @throws IllegalArgumentException if the string contains non-ASCII or non-uppercase characters
+   */
+  public static void requireUppercaseAscii(final String string,
+                                           final boolean acceptNullTerminators) {
     requireNonNull(string);
     for (int i = 0; i < string.length(); i++) {
       char c = string.charAt(i); // ignore code points outside of the BMP
+      if (acceptNullTerminators && c == '\0') {
+        continue;
+      }
       if (c != ' ' && (c < 'A' || c > 'Z')) {
         throw new IllegalArgumentException("Expected all-uppercase ASCII string, got " + string);
       }
     }
+  }
+
+  /**
+   * Returns a string of the given length, consisting of {@code string} followed by as
+   * many instances of {@code padChar} as necessary to reach that length.
+   *
+   * @param string the string to pad
+   * @param length the length of the returned string
+   * @param padChar the character to append at the end of the string
+   * @return the padded string
+   * @throws IllegalArgumentException if {@code string.length() > length}
+   */
+  // TODO Change all string setters in GB(A)Cartridge to pad if necessary using this method
+  public static String padEnd(final String string, int length, char padChar) {
+    final int originalLength = string.length();
+    if (originalLength > length) {
+      throw new IllegalArgumentException(
+        "Cannot pad string with length greater than padded length");
+    }
+    return string + String.valueOf(padChar).repeat(length - originalLength);
   }
 
   private StringUtils() {
