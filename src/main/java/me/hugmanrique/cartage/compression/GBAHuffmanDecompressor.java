764d93817a7444a9005898585b788e1cba23d0b4
Hugo Manrique <contact@hugmanrique.me>
2021-06-22 19:54:15 +0200

    Huffman decompression

--- src/main/java/me/hugmanrique/cartage/compression/GBAHuffmanDecompressor.java
+++ src/main/java/me/hugmanrique/cartage/compression/GBAHuffmanDecompressor.java
@@ -30,8 +30,11 @@ public final class GBAHuffmanDecompressor implements Decompressor {
   }
 
   private static final byte MAGIC_NUMBER = 0x20;
-  private static final int DATA_SIZE = 0xF;
+  private static final int BIT_DEPTH = 0xF;
   private static final int DECOMPRESSED_LENGTH = 0xFFFFFF;
+  private static final int CHILD_IS_LEAF = 0x80;
+  private static final int CHILD_OFFSET = 0x3F;
+  private static final long ALIGN_BASE_OFFSET = ~0x1;
 
   private GBAHuffmanDecompressor() {}
 
@@ -41,58 +44,50 @@ public final class GBAHuffmanDecompressor implements Decompressor {
       final int header = cartridge.readInt();
       checkCompressionType(header, MAGIC_NUMBER, "HF");
 
-      final int bitDepth = (header >> 28) & DATA_SIZE;
-      final int length = header & DECOMPRESSED_LENGTH;
-      final int treeSize = (cartridge.readByte() + 1) * 2;
-      final byte[] result = new byte[length];
-
-      final long rootNodeOffset = cartridge.offset();
-      long nodeOffset = rootNodeOffset;
-      cartridge.skip(treeSize);
-
       // The compressed data contains a binary tree and a set of paths starting at
       // the root node of the tree, encoded as a sequence of 32-bit integers.
       // The bits of the integer indicate whether to follow the left node (0) or
       // the right node (1), starting at the most-significant bit. A path ends
-      // when a leaf node is reached, which contains an uncompressed symbol.
-      // This traversal process is repeated with the remaining bits of the integer.
-      for (int index = 0; index < length; index++) {
-        final int slice = cartridge.getInt(treeSize + index);
+      // when a leaf node is reached, which contains an uncompressed value of
+      // bitDepth bits (usually 4 or 8).
 
-        for (int i = 31; i >= 0; i--) {
-          final int goLeft = (slice >>> i) & 0x1;
-          final byte nodeData = cartridge.getByte(nodeOffset);
-          final boolean nextIsLeaf = ((nodeData << goLeft) & 0x80) != 0;
-          //nodeOffset =
+      final int bitDepth = (header >> 28) & BIT_DEPTH;
+      final int length = header & DECOMPRESSED_LENGTH;
+      final byte[] result = new byte[length];
 
+      final int treeLength = (cartridge.readByte() + 1) << 1;
+      final long rootNodeOffset = cartridge.offset();
+      final long pathsOffset = rootNodeOffset + treeLength;
+
+      int index = 0;
+      byte currentCount = 0; // number of bits written to result[index]
+      while (index < length) {
+        final int paths = cartridge.getInt(pathsOffset + index);
+
+        for (byte i = 31; i >= 0; i--) {
+          final int direction = (paths >>> i) & 0x1; // left or right child
+          final byte node = cartridge.readByte();
+
+          final int nextDelta = (((node & CHILD_OFFSET) + 1) << 1) | direction;
+          final long nextOffset = (cartridge.offset() & ALIGN_BASE_OFFSET) + nextDelta;
+          cartridge.setOffset(nextOffset);
+
+          // If the 7th (6th) bit is set, the left (right) child is a leaf
+          final boolean nextIsLeaf = ((node << direction) & CHILD_IS_LEAF) != 0;
           if (nextIsLeaf) {
+            final byte value = cartridge.readByte();
+            result[index] = (byte) ((result[index] << bitDepth) | value);
+            currentCount += bitDepth;
+            if (currentCount == 8) {
+              index++;
+            }
+
+            cartridge.setOffset(rootNodeOffset);
           }
         }
-
-
-        /*for (int i = 0; i < 32; i++, slice <<= 1) {
-          final boolean left = (slice & 31) != 0;
-          final byte treeData = cartridge.readByte();
-          //final boolean nextIsLeaf = treeData & ()
-        }*/
       }
 
-      /*int index = 0;
-
-      while (index < length) {
-        final int path = cartridge.readInt();
-        for (int i = 0; i < Integer.BYTES * 8; i++) {
-
-        }
-
-
-        //final int value = cartridge.readInt();
-
-      }*/
-
-
-
-
+      cartridge.setOffset(pathsOffset + length); // end of sequence of paths
       return result;
     } catch (final IndexOutOfBoundsException e) {
       throw new DecompressionException("Got corrupted Huffman-compressed data");
